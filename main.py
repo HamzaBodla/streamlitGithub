6806699ae451a21d0b1e2208ac6956bc682ee405
HamzaBodla <hamzabodla97@gmail.com>
2024-03-20 02:35:07 +0000

    code updated

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import base64
 import pandas as pd
 import matplotlib.pyplot as plt
 import cv2
-from streamlit_webrtc import webrtc_streamer, RTCConfiguration
+# from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 import av
+from ultralytics import YOLO
  
 csv_file_path = "counts_vid_seconds.csv"
 df = pd.read_csv(csv_file_path)
-x = 4
+
+model = YOLO("yolov8n-seg.pt")
+
+
  
 # Add a custom sidebar layout with logo and colored border
 st.markdown(
@@ -34,12 +38,94 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+
+
+
+
+
+
+def _display_detected_frames(conf, model, st_frame, image):
+    """
+    Display the detected objects on a video frame using the YOLOv8 model.
+
+    Args:
+    - conf (float): Confidence threshold for object detection.
+    - model (YoloV8): A YOLOv8 object detection model.
+    - st_frame (Streamlit object): A Streamlit object to display the detected video.
+    - image (numpy array): A numpy array representing the video frame.
+    - is_display_tracking (bool): A flag indicating whether to display object tracking (default=None).
+
+    Returns:
+    None
+    """
+
+    # Resize the image to a standard size
+    image = cv2.resize(image, (720, int(720*(9/16))))
+
+    res = model.predict(image, conf=conf)
+
+    # # Plot the detected objects on the video frame
+    res_plotted = res[0].plot()
+    st_frame.image(res_plotted,
+                   caption='Detected Video',
+                   channels="BGR",
+                   use_column_width=True
+                   )
+    
+
+
+
+
+
+
+def play_webcam(conf, model):
+    """
+    Plays a webcam stream. Detects Objects in real-time using the YOLOv8 object detection model.
+
+    Parameters:
+        conf: Confidence of YOLOv8 model.
+        model: An instance of the `YOLOv8` class containing the YOLOv8 model.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    if st.sidebar.button('Detection Using Webcam'):
+        try:
+            vid_cap = cv2.VideoCapture(0)
+            st_frame = st.empty()
+            while (vid_cap.isOpened()):
+                success, image = vid_cap.read()
+                if success:
+                    _display_detected_frames(conf,
+                                             model,
+                                             st_frame,
+                                             image,
+                                             )
+                else:
+                    vid_cap.release()
+                    break
+        except Exception as e:
+            st.sidebar.error("Error loading video: " + str(e))
+    elif st.sidebar.button('Quit Detection'):
+        vid_cap = cv2.VideoCapture(0)
+        vid_cap.release()
+        
+
+
+
+
  
 # Function to convert image to base64
 def img_to_base64(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
  
+
+
+
 # Load and display sidebar image with glowing effect
 img_path = "smartislogo.png"
 img_base64 = img_to_base64(img_path)
@@ -47,55 +133,34 @@ st.sidebar.markdown(
     f'<img src="data:image/png;base64,{img_base64}" class="cover-glow">',
     unsafe_allow_html=True,
 )
+
 st.sidebar.markdown("---")
+
+
+
  
 # Sidebar for Mode Selection
 mode = st.sidebar.radio("Select Mode:", options=["Live detection", "Single angle", "Multiple angles", "Data insight"], index=0)
 st.sidebar.markdown("---")
+
+
  
 if mode == "Live detection":
-
-    def video_frame_callback(frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        
-
-
-        return av.VideoFrame.from_ndarray( format="bgr24")
-
-
-    webrtc_streamer(key="example", video_frame_callback=video_frame_callback, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
-
-
-
     st.title("Webcam Live Feed")
-    run = st.checkbox('Run')
-    FRAME_WINDOW = st.image([])
- 
-    # OpenCV camera setup
-    camera = cv2.VideoCapture(0)
- 
-    while run:
-        ret, frame = camera.read()
-        if not ret:
-            st.write("Error: Unable to capture image from the camera.")
-            break
- 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        FRAME_WINDOW.image(frame)
- 
-    camera.release()  # Release the camera when done
-    st.write('Stopped')
+    play_webcam(0.5, model)
+    
  
 elif mode == "Single angle":
     st.title("Object Detection and Tracking - Single angle")
     vid1 = "output_video.mp4"
     st.video(vid1)
+
  
 elif mode == "Multiple angles":
     st.title("Object Detection and Tracking - Multiple angles")
     vid2 = "hall1hall2.mp4"
     st.video(vid2)
+    
  
 elif mode == "Data insight":
     st.title("Object Detection and Tracking - Data Insight")
